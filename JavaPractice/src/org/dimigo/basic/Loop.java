8d9ec08879d11c0909218353842a3dd64422204b
wp132332 <studentncube@dimigo.hs.kr>
2015-03-26 15:15:58 +0900

    다섯번째 실습과제 제출

---
+++ JavaPractice/src/org/dimigo/basic/Loop.java
@@ -0,0 +1,15 @@
+package org.dimigo.basic;
+
+public class Loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 1; i <= 10; i++) {
+			for (int j = 1; j <= 10; j++) {
+				if(i!=j)System.out.print(j);
+				else System.out.print("*");
+			}
+			System.out.println("");
+		}
+	}
+}
