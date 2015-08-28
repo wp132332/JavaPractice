488ed56f8a11e1b1f22c4aaa994564715c0825a8
MING <MING@MING-PC>
2015-08-28 14:28:12 +0900

    실습 과제 스마트폰

---
+++ JavaPractice/src/org/dimigo/inheritance/IPhone.java
@@ -0,0 +1,31 @@
+package org.dimigo.inheritance;
+
+/*
+ *
+ * <pre>
+ * org.dimigo.inheritance
+ *		|_ IPhone
+ *
+ * 1. 개요 : 
+ * 2. 작성일 : 2015. 8. 28.
+ * </pre>
+ * @User				: MING
+ * @author			: 조준희
+ * @version			: 1.0
+ */
+public class IPhone extends SmartPhone {
+	public IPhone() {
+		this("아이폰","애플",700000);
+	}
+
+	public IPhone(String model, String company, int price) {
+		super(model, company, price);
+	}
+	
+	public void pay(){
+		System.out.println("애플 페이로 결제합니다.");
+	}
+	public void useAirDrop(){
+		System.out.println("AirDrop 기능을 사용합니다.");
+	}
+}
